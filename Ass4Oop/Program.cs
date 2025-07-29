b53cc761ae030ef26ccbb87ba96d24f207da43e2
Admin <Admin@SALMAAMRO>
2025-07-29 17:55:14 +0300

    Q1

--- Ass4Oop/Program.cs
+++ Ass4Oop/Program.cs
@@ -1,12 +1,70 @@
 ﻿namespace Ass4Oop
 {
-   
+    #region Q1 
+    //interface IShape 
+    //{
+    //    public double Area { get; }
+    //    public void DisplayShapeInfo();
+    //};
+
+    //interface ICircle : IShape
+    //{
+    //    public double Raduis { get; set; }
+    //};
+
+    //interface IRectangle : IShape
+    //{
+    //    public double Length { get; set; }
+    //    public double Width { get; set; }
+    //};
+    //class Circle : ICircle , IShape
+    //{
+    //    double area;
+    //    public double Area
+    //    {
+    //        get { return 3.14 * Raduis * Raduis; }
+    //    }
+    //  public double Raduis { get; set; }
+
+    //  public void DisplayShapeInfo()
+    //  {
+    //        Console.WriteLine($"Circle with Radius: {Raduis} and Area: {Area}");
+    //  }
+
+    //}
+    //class Rectangle : IRectangle, IShape
+    //{
+    //    double area;
+    //   public double Length { get; set; }
+    //   public double Width { get; set; }
+
+    //    public double Area
+    //    {
+    //        get { return Length * Width; }
+    //    }
+    //    public void DisplayShapeInfo()
+    //    {
+    //        Console.WriteLine($"Rectangle with Length: {Length}, Width: {Width} and Area: {Area}");
+    //    }
+
+    //}
+    #endregion
     class Program
     {
      
         static void Main(string[] args)
         {
-          
+            #region CallQ1
+            //Console.WriteLine("The Area Of Circle");
+            //Circle C1 = new Circle();
+            //C1.Raduis = 5;
+            //C1.DisplayShapeInfo();
+            //Console.WriteLine("The Area Of Rectangle");
+            //Rectangle R1 = new Rectangle();
+            //R1.Length = 6;
+            //R1.Width = 12;
+            //R1.DisplayShapeInfo();
+            #endregion
         }
     }
 }
